8c7853682948fadca0d41ab3cac94c4747cef40b
mervezgin <mervezgin@gmail.com>
2024-08-01 12:29:31 +0300

    added player and PlayerController.cs to player
    Player is moving properly

---
+++ Assets/_Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed = 7.0f;
+    private float rotateSpeed = 10f;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
+        inputVector = inputVector.normalized;
+        Vector3 moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+    }
+}
